cffb6ef566d1192c2a887b846642664ffceb890f
JesusAlonsosan <jesusalonsosan@gmail.com>
2019-05-08 21:59:26 -0500

    Upgrading dependencies. and :white_check_mark:

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
+// Check node --version (terminal)
+// Check npm ---version (terminal)
+// Install npm install -g create-react-app (terminal)
+// Create new React app whit create-react-app nameapp (terminal)
+// cd nameapp and  typing  npm start (terminal)
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+var element = React.createElement('h1', { className: 'greeting' },
+    'Без тени сомненья, без капли печали.!');
+
+
+ReactDOM.render(element, document.getElementById('root'));
 serviceWorker.unregister();
